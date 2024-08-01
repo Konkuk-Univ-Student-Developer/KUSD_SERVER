622ae9a25501e1e599813c8abbe833c9eaf7d205
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-08-01 15:53:53 +0900

    [chore]프로젝트 패키지 구성

---
+++ src/main/java/com/kusd/KUmap/KUmapApplication.java
@@ -0,0 +1,13 @@
+package com.kusd.KUmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KUmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KUmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kusd/KUmap/KUmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusd.KUmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KUmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
