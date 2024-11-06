a0eff99ead22f4843a73b474e6a1138c5f405885
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-11-06 16:55:48 +0900

    [fix]: 학과 경쟁률 평균으로 변경

--- src/main/java/com/kusd/KUmap/domain/competitionRate/dto/response/CompetitionRateResponse.java
+++ src/main/java/com/kusd/KUmap/domain/competitionRate/dto/response/CompetitionRateResponse.java
@@ -1,62 +1,23 @@
 package com.kusd.KUmap.domain.competitionRate.dto.response;
 
 import com.kusd.KUmap.domain.competitionRate.domain.CompetitionRate;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Map;
 
 public record CompetitionRateResponse(
     String year,
+    @Schema(example = "BBAB54724")
     String haksuId,
-    String professor,
-    String totalApplicationNumber,
-    String undergraduateNumber,
-    String postgraduateNumber,
-    String lectureTime,
-    Map<String, Double> total,
-    Map<String, Double> freshman,
-    Map<String, Double> sophomore,
-    Map<String, Double> junior,
-    Map<String, Double> senior
+    int totalApplicationNumber,
+    Map<String, Double> competitionRates
 ) {
 
-    public static CompetitionRateResponse from(CompetitionRate competitionRate) {
+    public static CompetitionRateResponse from(CompetitionRate entity, int totalApplicationNumber, Map<String, Double> competitionRates) {
         return new CompetitionRateResponse(
-                competitionRate.getYear(),
-                competitionRate.getHaksuId(),
-                competitionRate.getProfessor(),
-                competitionRate.getTotalApplicationNumber(),
-                competitionRate.getUndergraduateNumber(),
-                competitionRate.getPostgraduateNumber(),
-                competitionRate.getLectureTime(),
-                Map.of(
-                        "totalNumber", (double) competitionRate.getTotalNumber(),
-                        "totalCourseBasketNumber", (double) competitionRate.getTotalCourseBasketNumber(),
-                        "totalCompetitionRate", competitionRate.getTotalCompetitionRate()
-                        // assuming this is already double
-                ),
-                Map.of(
-                        "freshmanTotalNumber", (double) competitionRate.getFreshmanTotalNumber(),
-                        "freshmanCourseBasketNumber", (double) competitionRate.getFreshmanCourseBasketNumber(),
-                        "freshmanCompetitionRate", competitionRate.getFreshmanCompetitionRate()
-                        // assuming this is already double
-                ),
-                Map.of(
-                        "sophomoreTotalNumber", (double) competitionRate.getSophomoreTotalNumber(),
-                        "sophomoreCourseBasketNumber", (double) competitionRate.getSophomoreCourseBasketNumber(),
-                        "sophomoreCompetitionRate", competitionRate.getSophomoreCompetitionRate()
-                        // assuming this is already double
-                ),
-                Map.of(
-                        "juniorTotalNumber", (double) competitionRate.getJuniorTotalNumber(),
-                        "juniorCourseBasketNumber", (double) competitionRate.getJuniorCourseBasketNumber(),
-                        "juniorCompetitionRate", competitionRate.getJuniorCompetitionRate()
-                        // assuming this is already double
-                ),
-                Map.of(
-                        "seniorTotalNumber", (double) competitionRate.getSeniorTotalNumber(),
-                        "seniorCourseBasketNumber", (double) competitionRate.getSeniorCourseBasketNumber(),
-                        "seniorCompetitionRate", competitionRate.getSeniorCompetitionRate()
-                        // assuming this is already double
-                )
+                entity.getYear(),
+                entity.getHaksuId(),
+                totalApplicationNumber,
+                competitionRates
         );
     }
 }
--- src/main/java/com/kusd/KUmap/domain/competitionRate/service/CompetitionRateService.java
+++ src/main/java/com/kusd/KUmap/domain/competitionRate/service/CompetitionRateService.java
@@ -3,20 +3,72 @@ package com.kusd.KUmap.domain.competitionRate.service;
 import com.kusd.KUmap.domain.competitionRate.domain.CompetitionRate;
 import com.kusd.KUmap.domain.competitionRate.dto.response.CompetitionRateResponse;
 import com.kusd.KUmap.domain.competitionRate.repository.CompetitionRateRepository;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CompetitionRateService {
+    private static final double ROUNDING_FACTOR = 1000.0;
 
     private final CompetitionRateRepository competitionRateRepository;
 
-    public List<CompetitionRateResponse> getCompetitionRate(String haksuId) {
-        return competitionRateRepository.findByHaksuId(haksuId).stream()
-                .map(CompetitionRateResponse::from)
-                .toList();
+    public CompetitionRateResponse getCompetitionRate(String haksuId) {
+        List<CompetitionRate> competitionRates = competitionRateRepository.findByHaksuId(haksuId);
+        int totalApplicationNumber = getTotalApplicationNumber(competitionRates);
+        Map<String, Double> averageTotalCompetitionRate = calculateAverageCompetitionRate(competitionRates);
+
+        return  CompetitionRateResponse.from(competitionRates.get(0), totalApplicationNumber, averageTotalCompetitionRate);
     }
 
+    private static int getTotalApplicationNumber(List<CompetitionRate> competitionRates) {
+        return competitionRates.stream()
+                .map(CompetitionRate::getUndergraduateNumber)
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    private Map<String, Double> calculateAverageCompetitionRate(List<CompetitionRate> competitionRates) {
+        Map<String, Double> averageCompetitionRate = new HashMap<>();
+
+        for (CompetitionRate competitionRate : competitionRates) {
+            // Total
+            double totalNum = sumNumber("totalNumber", competitionRate.getTotalNumber(), averageCompetitionRate);
+            double totalBasketNum = sumNumber("totalCourseBasketNumber", competitionRate.getTotalCourseBasketNumber(), averageCompetitionRate);
+            averageCompetitionRate.put("totalCompetitionRate", getCompetitionRate(totalBasketNum, totalNum));
+
+            // Freshman
+            double freshmanNum = sumNumber("freshmanTotalNumber", competitionRate.getFreshmanTotalNumber(), averageCompetitionRate);
+            double freshmanBasketNum = sumNumber("freshmanCourseBasketNumber", competitionRate.getFreshmanCourseBasketNumber(), averageCompetitionRate);
+            averageCompetitionRate.put("freshmanCompetitionRate", getCompetitionRate(freshmanBasketNum, freshmanNum));
+
+            // Sophomore
+            double sophomoreNum = sumNumber("sophomoreTotalNumber", competitionRate.getSophomoreTotalNumber(), averageCompetitionRate);
+            double sophomoreBasketNum = sumNumber("sophomoreCourseBasketNumber", competitionRate.getSophomoreCourseBasketNumber(), averageCompetitionRate);
+            averageCompetitionRate.put("sophomoreCompetitionRate", getCompetitionRate(sophomoreBasketNum, sophomoreNum));
+
+            // Junior
+            double juniorNum = sumNumber("juniorTotalNumber", competitionRate.getJuniorTotalNumber(), averageCompetitionRate);
+            double juniorBasketNum = sumNumber("juniorCourseBasketNumber", competitionRate.getJuniorCourseBasketNumber(), averageCompetitionRate);
+            averageCompetitionRate.put("juniorCompetitionRate", getCompetitionRate(juniorBasketNum, juniorNum));
+
+            // Senior
+            double seniorNum = sumNumber("seniorTotalNumber", competitionRate.getSeniorTotalNumber(), averageCompetitionRate);
+            double seniorBasketNum = sumNumber("seniorCourseBasketNumber", competitionRate.getSeniorCourseBasketNumber(), averageCompetitionRate);
+            averageCompetitionRate.put("seniorCompetitionRate", getCompetitionRate(seniorBasketNum, seniorNum));
+        }
+        return averageCompetitionRate;
+    }
+
+    private static double getCompetitionRate(double totalCourseBasketNumber, double sumTotalNum) {
+        double v = totalCourseBasketNumber / sumTotalNum;
+        return Math.round(v * ROUNDING_FACTOR) / ROUNDING_FACTOR;
+    }
+
+    private static double sumNumber(String key, int value, Map<String, Double> averageCompetitionRate) {
+        return averageCompetitionRate.getOrDefault(key, 0.0) + value;
+    }
 }
