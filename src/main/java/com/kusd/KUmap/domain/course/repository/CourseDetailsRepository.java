3ae2747a0e9994404c0aa2ba060321bef1959c6c
박세준 <74056843+sejoon00@users.noreply.github.com>
2024-12-01 18:52:37 +0900

    [fix] 승인된 교과목만 검색되게 쿼리 수정

--- src/main/java/com/kusd/KUmap/domain/course/repository/CourseDetailsRepository.java
+++ src/main/java/com/kusd/KUmap/domain/course/repository/CourseDetailsRepository.java
@@ -9,18 +9,17 @@ import org.springframework.data.repository.query.Param;
 
 public interface CourseDetailsRepository extends JpaRepository<CourseDetails, String> {
 
-    @Query("SELECT c FROM CourseDetails c WHERE c.competencyInCourse.competencyCode1 = :cmptCode OR c.competencyInCourse.competencyCode2 = :cmptCode OR c"
-        + ".competencyInCourse.competencyCode3 = :cmptCode ")
+    @Query("SELECT c FROM CourseDetails c WHERE (c.competencyInCourse.competencyCode1 = :cmptCode OR c.competencyInCourse.competencyCode2 = :cmptCode OR c.competencyInCourse.competencyCode3 = :cmptCode) AND c.applicationStatusCode = '승인'")
     List<CourseDetails> findAllByCompetencyCode(@Param("cmptCode") String cmptCode);
 
-    @Query("SELECT c FROM CourseDetails c WHERE c.addInformation.openingSubjectCode = :openingSubjectCode")
+    @Query("SELECT c FROM CourseDetails c WHERE c.addInformation.openingSubjectCode = :openingSubjectCode AND c.applicationStatusCode = '승인'")
     List<CourseDetails> findAllByOpeningSubjectCode(@Param("openingSubjectCode") String openingSubjectCode);
 
     @Query("SELECT c FROM CourseDetails c WHERE c.addInformation.openingSubjectCode = :openingSubjectCode AND (c.competencyInCourse"
-        + ".competencyCode1 = :cmptCode OR c.competencyInCourse.competencyCode2 = :cmptCode OR c.competencyInCourse.competencyCode3 = :cmptCode)")
+            + ".competencyCode1 = :cmptCode OR c.competencyInCourse.competencyCode2 = :cmptCode OR c.competencyInCourse.competencyCode3 = :cmptCode)")
     List<CourseDetails> findAllByOpeningSubjectCodeAndCompetencyCode(
-        @Param("openingSubjectCode") String openingSubjectCode,
-        @Param("cmptCode") String cmptCode
+            @Param("openingSubjectCode") String openingSubjectCode,
+            @Param("cmptCode") String cmptCode
     );
 
     Optional<CourseDetails> findByHaksuId(String haksuId);
